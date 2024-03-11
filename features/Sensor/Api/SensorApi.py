9bcd15885bb1c1f71a21c0512c0335443f1a175e
harutiro <harutiro2027@outlook.jp>
2024-03-12 01:53:16 +0900

    sensorデータの読み取り方を変更

--- features/Sensor/Api/SensorApi.py
+++ features/Sensor/Api/SensorApi.py
@@ -35,9 +35,15 @@ class SensorApi:
 
 
     def print_latest_data(self,data:bytes) -> SensorData:
+
         """
         print measured latest value.
         """
+
+        if len(data) < 56:  # 最大のインデックス値に基づく
+            print("Data array is too short.")
+            return SensorData()
+
         time_measured = str(DateUtils.now_timestamp())
         temperature = self.s16(int(hex(data[9]) + '{:02x}'.format(data[8], 'x'), 16)) / 100
         relative_humidity = int(hex(data[11]) + '{:02x}'.format(data[10], 'x'), 16) / 100
@@ -55,66 +61,44 @@ class SensorApi:
         temperature_flag = int(hex(data[36]) + '{:02x}'.format(data[35], 'x'), 16)
         relative_humidity_flag = int(hex(data[38]) + '{:02x}'.format(data[37], 'x'), 16)
         ambient_light_flag = int(hex(data[40]) + '{:02x}'.format(data[39], 'x'), 16)
+        barometric_pressure_flag = int(hex(data[42]) + '{:02x}'.format(data[41], 'x'), 16)
+        sound_noise_flag = int(hex(data[44]) + '{:02x}'.format(data[43], 'x'), 16)
+        etvoc_flag = int(hex(data[46]) + '{:02x}'.format(data[45], 'x'), 16)
+        eco2_flag = int(hex(data[48]) + '{:02x}'.format(data[47], 'x'), 16)
+        discomfort_index_flag = int(hex(data[50]) + '{:02x}'.format(data[49], 'x'), 16)
+        heat_stroke_flag = int(hex(data[52]) + '{:02x}'.format(data[51], 'x'), 16)
+        si_value_flag = int(hex(data[53]), 16)
+        pga_flag = int(hex(data[54]), 16)
+        seismic_intensity_flag = int(hex(data[55]), 16)
 
-        if len(data) > 17:
-            barometric_pressure_flag = int(hex(data[42]) + '{:02x}'.format(data[41], 'x'), 16)
-            sound_noise_flag = int(hex(data[44]) + '{:02x}'.format(data[43], 'x'), 16)
-            etvoc_flag = int(hex(data[46]) + '{:02x}'.format(data[45], 'x'), 16)
-            eco2_flag = int(hex(data[48]) + '{:02x}'.format(data[47], 'x'), 16)
-            discomfort_index_flag = int(hex(data[50]) + '{:02x}'.format(data[49], 'x'), 16)
-            heat_stroke_flag = int(hex(data[52]) + '{:02x}'.format(data[51], 'x'), 16)
-            si_value_flag = int(hex(data[53]), 16)
-            pga_flag = int(hex(data[54]), 16)
-            seismic_intensity_flag = int(hex(data[55]), 16)
-
-            return SensorData(
-                time_measured=time_measured,
-                temperature=temperature,
-                relative_humidity=relative_humidity,
-                ambient_light=ambient_light,
-                barometric_pressure=barometric_pressure,
-                sound_noise=sound_noise,
-                eTVOC=eTVOC,
-                eCO2=eCO2,
-                discomfort_index=discomfort_index,
-                heat_stroke=heat_stroke,
-                vibration_information=vibration_information,
-                si_value=si_value,
-                pga=pga,
-                seismic_intensity=seismic_intensity,
-                temperature_flag=temperature_flag,
-                relative_humidity_flag=relative_humidity_flag,
-                ambient_light_flag=ambient_light_flag,
-                barometric_pressure_flag=barometric_pressure_flag,
-                sound_noise_flag=sound_noise_flag,
-                etvoc_flag=etvoc_flag,
-                eco2_flag=eco2_flag,
-                discomfort_index_flag=discomfort_index_flag,
-                heat_stroke_flag=heat_stroke_flag,
-                si_value_flag=si_value_flag,
-                pga_flag=pga_flag,
-                seismic_intensity_flag=seismic_intensity_flag
-            )
-        else:
-            return SensorData(
-                time_measured=time_measured,
-                temperature=temperature,
-                relative_humidity=relative_humidity,
-                ambient_light=ambient_light,
-                barometric_pressure=barometric_pressure,
-                sound_noise=sound_noise,
-                eTVOC=eTVOC,
-                eCO2=eCO2,
-                discomfort_index=discomfort_index,
-                heat_stroke=heat_stroke,
-                vibration_information=vibration_information,
-                si_value=si_value,
-                pga=pga,
-                seismic_intensity=seismic_intensity,
-                temperature_flag=temperature_flag,
-                relative_humidity_flag=relative_humidity_flag,
-                ambient_light_flag=ambient_light_flag
-            )
+        return SensorData(
+            time_measured=time_measured,
+            temperature=temperature,
+            relative_humidity=relative_humidity,
+            ambient_light=ambient_light,
+            barometric_pressure=barometric_pressure,
+            sound_noise=sound_noise,
+            eTVOC=eTVOC,
+            eCO2=eCO2,
+            discomfort_index=discomfort_index,
+            heat_stroke=heat_stroke,
+            vibration_information=vibration_information,
+            si_value=si_value,
+            pga=pga,
+            seismic_intensity=seismic_intensity,
+            temperature_flag=temperature_flag,
+            relative_humidity_flag=relative_humidity_flag,
+            ambient_light_flag=ambient_light_flag,
+            barometric_pressure_flag=barometric_pressure_flag,
+            sound_noise_flag=sound_noise_flag,
+            etvoc_flag=etvoc_flag,
+            eco2_flag=eco2_flag,
+            discomfort_index_flag=discomfort_index_flag,
+            heat_stroke_flag=heat_stroke_flag,
+            si_value_flag=si_value_flag,
+            pga_flag=pga_flag,
+            seismic_intensity_flag=seismic_intensity_flag
+        )
     
 
     def set_led(self, ser:serial.Serial , r:int, g:int, b:int) -> None:
